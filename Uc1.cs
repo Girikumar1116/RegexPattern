0d53a39e587a220b7c7b4660ca99098f7f3dc63b
girikumar <girikumar1116@gmail.com>
2023-05-08 16:06:48 +0530

    firstname pattern

---
+++ Uc1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace RegexPatterns
+{
+    class Uc1
+    {
+        static string NamePattern = @"^[A-Z][A-Za-z]{3,19}$";
+        public static void Name()
+        {
+            Console.WriteLine("enter a name");
+            var name = Console.ReadLine();
+            var flag = Regex.IsMatch(name, NamePattern);
+            if (flag)
+                Console.WriteLine("valid");
+            else
+                Console.WriteLine("invalid ");
+        }
+        static void Main(string[] args)
+        {
+            Name();
+        }
+    }
+}
