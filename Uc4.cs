302a6121b737fc6b4c88f8d2ba37a19becafdbf7
girikumar <girikumar1116@gmail.com>
2023-05-08 17:01:22 +0530

    [mobile format]

---
+++ Uc4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPatterns
+{
+    public class Uc4
+    {
+        static string NumberPattern = @"^[0-9]{2}(\s)[0-9]{10}$";
+        public static void PhoneNumber()
+        {
+            Console.WriteLine("enter a mobile number");
+            var mobileno = Console.ReadLine();
+            var flag = Regex.IsMatch(mobileno, NumberPattern);
+            if (flag)
+                Console.WriteLine("valid");
+            else
+                Console.WriteLine("invalid ");
+        }
+        public static void Main(string[] args)
+        {
+            PhoneNumber();
+        }
+    }
+}
